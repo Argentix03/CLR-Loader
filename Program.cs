4cc4330938814de0d1f80b3fe15cd771169929dc
Cheka <39255806+Argentix03@users.noreply.github.com>
2022-04-26 02:41:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace ShowArgsMsgBox
+{
+    class Program
+    {
+        public static int randomFunction(string arguments)
+        {
+            MessageBox.Show(arguments);
+            return 0;
+        }
+        static void Main()
+        {
+            // defined main
+        }
+    }
+}
